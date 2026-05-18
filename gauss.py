d36391ca2cc3d44c7f3dc72d33da350bb443610d
JeremyWu0207 <106864061+JeremyWu0207@users.noreply.github.com>
2026-05-18 20:44:48 +0800

    Refactor comments and clarify zero threshold logic
    Removed unnecessary comments and clarified zero threshold condition.

--- gauss.py
+++ gauss.py
@@ -8,6 +8,7 @@ def row_interchange(R, i, j):
     # We use clone() to ensure we don't have reference issues during swapping
     temp = R[i].clone()
     # TODO: Perform row interchange
+    # (Should done in HW1, not necessary to report)
     R[i] = R[j].clone()
     R[j] = temp
     return R
@@ -19,6 +20,7 @@ def row_scaling(R, i, s):
     # Hint: To use the scalar_matrix, a row of 1D tensor should be reshaped to a row 2D tensor.
     # For example, R[i] should be reshaped as (1, cols) to fit the function requirments
     # Hint: You may use API flatten() to convert a 2D tensor back to 1D tensor
+    # (Should done in HW1, not necessary to report)
     row_2d = R[i].reshape(1, cols)
     scaled_row_2d = scalar_matrix(s, row_2d)
     R[i] = scaled_row_2d.flatten()
@@ -32,6 +34,7 @@ def row_addition(R, i, j, s):
     # 2D tensor.
     # For example, R[i] should be reshaped as (1, cols) to fit the function requirments
     # Hint: You may use API flatten() to convert a 2D tensor back to 1D tensor
+    # (Should done in HW1, not necessary to report)
     row_i_2d = R[i].reshape(1, cols)
     row_j_2d = R[j].reshape(1, cols)
     scaled_i_2d = scalar_matrix(s, row_i_2d)
@@ -51,29 +54,12 @@ def gauss_elimination(A):
     pivot_row = 0
     pivot_col = 0
 
-    # Set zero thresh (if entry is larger than this value, treat it as zero. Otherwise, treat it as nonzero)
+    # Set zero thresh (if entry is smaller than this value, treat it as zero. Otherwise, treat it as nonzero)
     zero_thresh = 1e-6
 
     # TODO: Implement step 1 to step 4 in the lecture slides (the forward phase to Row Echelon Form) 
-    '''講義內容
-    Step 1.
-    Determine the left most nonzero column.
-    • This is a pivot column
-    • The topmost position in this column is a pivot position
-
-    Step 2.
-    In the pivot column, choose any nonzero entry in a row that is not above the pivot row, and
-    perform the appropriate row interchange to bring this entry into the pivot position.
-
-    Step 3.
-    Perform appropriate row addition to each lower row in order to change each entry below
-    the pivot position into 0.
-
-    Step 4.
-    Ignore the row containing the pivot position and all rows above it. If there is a nonzero row
-    that is not ignored, repeat steps 1-4 on the submatrix that remains. 
-    '''
     # Hint: You should use our custom row_interchange, row_scaling, row_addition funtions
+    # (Should done in HW1, not necessary to report)
     while pivot_row < rows and pivot_col < cols:
         max_idx = pivot_row + torch.argmax(torch.abs(R[pivot_row:rows, pivot_col]))
         # 投影片寫找到一個非零，但我選擇找一個非零且取絕對值最大的，這樣可以增加數值穩定性
@@ -94,17 +80,9 @@ def gauss_elimination(A):
 
         pivot_row += 1
         pivot_col += 1
-
     # TODO: Implement step 5 and step 6 in the lecture slides (the backward phase to Reduced Row Echelon Form) 
-    '''講義內容
-    Step 5.
-    Scale the leading entry at the last pivot position to be 1. Then add an appropriate multiple of 
-    - this row to every preceding row so that each entry above the pivot position is 0.
-
-    Step 6.
-    If step 5 was performed on the first row, stop. Otherwise, repeat step 5 on the preceding
-    '''
     # Hint: You should use our custom row_interchange, row_scaling, row_addition funtions
+    # (Should done in HW1, not necessary to report)
     for i in range(rows - 1, -1, -1):
         pivot_idx = -1
         for j in range(cols):
